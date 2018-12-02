c4e3a1f0973706519623a27d7886dc03935456f4
Aleksandra Zakova <aleksandrazakova96@gmail.com>
2018-12-02 14:24:34 +0200

    Upload project to GitHub

---
+++ AskMe.Data/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using AskMe.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AskMe.Data
+{
+    public class ApplicationContext : IdentityDbContext<ApplicationUser>
+    {
+		public ApplicationContext(DbContextOptions options) : base(options)
+		{
+		}
+	}
+}
